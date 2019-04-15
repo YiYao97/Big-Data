ea64d1363276b109cdab8c7607635e7a1efe3dd8
YiYao97 <yi.yao97@icloud.com>
2019-04-15 15:15:27 +0000

    syntax errors checked

--- Docker/app.py
+++ Docker/app.py
@@ -1,7 +1,8 @@
-from flask import Flask, request, jsonify, make_response, redirect, url_for, abort
+from flask import Flask, request, jsonify, make_response,\
+        redirect, url_fo, abort
 from werkzeug.utils import secure_filename
 import os
-import tensorflow as tf
+from mnist_soft import predict
 
 
 UPLOAD_FOLDER = '/root/Docker'
@@ -24,8 +25,11 @@ numbers = [
         'done': False
     }
 ]
+
+
 def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() \
+            in ALLOWED_EXTENSIONS
 
 
 @app.route('/mnist/upload_image', methods=['GET', 'POST'])
@@ -41,6 +45,7 @@ def upload_file():
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             return redirect(url_for('uploaded_file', filename=filename))
     return jsonify({'upload_file': filename})
+
 
 @app.route('/numbers', methods=['GET'])
 def get_numbers():
@@ -59,9 +64,11 @@ def get_number(number_id):
 def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
 
+
 @app.errorhandler(400)
 def bad_request(error):
     return make_response(jsonify({'error': 'Bad Request'}), 400)
+
 
 @app.route('/numbers/upload', methods=['POST'])
 def upload_number():
@@ -69,12 +76,12 @@ def upload_number():
         abort(400)
     number = {
         'id': numbers[-1]['id'] + 1,
-        'title':request.json['title'],
-        'description':request.json.get('description', ""),
-        'done':False
+        'title': request.json['title'],
+        'description': request.json.get('description', ""),
+        'done': False
     }
     numbers.append(number)
-    return jsonify({'number':number}) , 201
+    return jsonify({'number': number}), 201
 
 
 @app.route('/numbers/<int:number_id>', methods=['DELETE'])
@@ -95,12 +102,14 @@ def update_number(number_id):
         abort(400)
     if 'title' in request.json and type(request.json['title']) != str:
         abort(400)
-    if 'description' in request.json and type(request.json['description']) is not str:
+    if 'description' in request.json and type(request.json['description']) \
+       is not str:
         abort(400)
     if 'done' in request.json and type(request.json['done']) is not bool:
         abort(400)
     number[0]['title'] = request.json.get('title', number[0]['title'])
-    number[0]['description'] = request.json.get('title', number[0]['description'])
+    number[0]['description'] = request.json.get('title',
+                                                number[0]['description'])
     number[0]['done'] = request.json.get('title', number[0]['done'])
     return jsonify({'number': number})
 
